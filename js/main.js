76199e6a3cbbdc87d2a7824fd2e42850f48e5a27
iam-rasulov <mirjalolrasulov002@gmail.com>
2022-02-20 12:56:51 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var result = Number ( prompt (`nechpuliz bor?` , 49999999));
+
+if(result >= 49999999){
+    console.log("Assalomu alaykum Umidbek Khaitbaev! Men Rasulov Mirjalol web-dasturlash standart 23-guruh o`quvchisi sizga shuni ma`lum qilmoqchimanki, oq yo`l -_*");
+}else if(result <= 4999999){
+    console.log("Assalomu alaykum Umidbek Khaitbaev Men Rasulov Mirjalol web-dasturlash standart 23-guruh o`quvchisi sizdan uzur so`ragan holatda sizga shuni ma`lum qilishim kerak, kechirasiz sizning hisobingizda mablag` yetarli emas. Iltimos hisobingizni to`lidiring! -_*");
+}
