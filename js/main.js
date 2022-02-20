b3614cd80fd94b9799a7a90ad5d3bc54b35785a9
iam-rasulov <mirjalolrasulov002@gmail.com>
2022-02-20 15:53:45 +0500

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-var result = Number ( prompt (`nechpuliz bor?` , 49999999));
+var result = prompt ("Summani kiriting");
 
-if(result >= 49999999){
+var a = (500);
+var b = (250);
+var c = (120);
+
+var x = (a *  10900)
+var y = (b *  10900)
+var z = (c *  12473)
+
+var xyz = (x + y + z);
+
+if(result >= xyz){
     console.log("Assalomu alaykum Umidbek Khaitbaev! Men Rasulov Mirjalol web-dasturlash standart 23-guruh o`quvchisi sizga shuni ma`lum qilmoqchimanki, oq yo`l -_*");
-}else if(result <= 4999999){
+}else{
     console.log("Assalomu alaykum Umidbek Khaitbaev Men Rasulov Mirjalol web-dasturlash standart 23-guruh o`quvchisi sizdan uzur so`ragan holatda sizga shuni ma`lum qilishim kerak, kechirasiz sizning hisobingizda mablag` yetarli emas. Iltimos hisobingizni to`lidiring! -_*");
 }
